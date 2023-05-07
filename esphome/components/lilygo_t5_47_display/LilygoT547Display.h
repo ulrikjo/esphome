b7e7c16c8f370dd257b66894cd721b36a212077f
ulrikjo <53315889+ulrikjo@users.noreply.github.com>
2023-05-07 18:58:52 +0200

    Update LilygoT547Display.h

--- esphome/components/lilygo_t5_47_display/LilygoT547Display.h
+++ esphome/components/lilygo_t5_47_display/LilygoT547Display.h
@@ -27,6 +27,7 @@ class LilygoT547Display : public PollingComponent, public display::DisplayBuffer
   float get_setup_priority() const override;
 
   void set_clear_screen(bool clear);
+  void set_clear_black_screen(bool clear_black);
   void set_landscape(bool landscape);
   void set_power_off_delay_enabled(bool power_off_delay_enabled);
   void set_temperature(uint32_t temperature);
@@ -40,8 +41,10 @@ class LilygoT547Display : public PollingComponent, public display::DisplayBuffer
   void update() override;
 
   void clear();
+  void clear_black();
   void flush_screen_changes();
   void set_all_white();
+  void set_all_black();
   void poweron();
   void poweroff();
   void on_shutdown() override;
@@ -54,6 +57,7 @@ class LilygoT547Display : public PollingComponent, public display::DisplayBuffer
   void HOT draw_absolute_pixel_internal(int x, int y, Color color) override;
 
   bool clear_;
+  bool clear_black_;
   bool init_clear_executed_ = false;
   bool temperature_;
   bool power_off_delay_enabled_;
